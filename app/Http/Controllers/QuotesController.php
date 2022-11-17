7afd9ac0aa2411184917605e530f050ce6a5f8e6
Adrian <sirianni.adrian@gmail.com>
2022-11-17 15:12:29 -0300

    Update QuotesController.php

--- app/Http/Controllers/QuotesController.php
+++ app/Http/Controllers/QuotesController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Quotes;
+use App\Models\User;
 
 class QuotesController extends Controller
 {
@@ -13,7 +16,8 @@ class QuotesController extends Controller
      */
     public function index()
     {
-        //
+        $token=\App\Validaciones::validateToken();
+        return $token["response"];
     }
 
     /**
@@ -35,6 +39,50 @@ class QuotesController extends Controller
     public function store(Request $request)
     {
         //
+        $token=\App\Validaciones::validateToken();
+        $datos=$request->all();
+
+        $validator = Validator::make($request->all(), [
+            'date' => 'required|date',
+            'detail' => 'required|string|max:255',
+            'id_professional' => [
+                'required',
+                'integer',
+                function ($attribute, $value, $fail) {
+                    $user=User::find($value);
+                    if ($user->id_type === 2) {
+                        $fail($attribute.' no type');
+                    }
+                },
+            ],
+            'id_visitor' => [
+                'required',
+                'integer',
+                function ($attribute, $value, $fail) {
+                    $user=User::find($value);
+                    if ($user->id_type === 1) {
+                        $fail($attribute.' no type');
+                    }
+                },
+            ]
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(),400);
+        }
+
+        $quote = new Quotes;
+        $quote->date=$datos["date"];
+        $quote->detail=$datos["detail"];
+        $quote->id_professional=$datos["id_professional"];
+        $quote->id_visitor=$datos["id_visitor"];
+        $quote->state=1;
+        $quote->save();
+       
+        $token["response"]=response()->json($quote,200);
+
+
+        return $token["response"];
     }
 
     /**
