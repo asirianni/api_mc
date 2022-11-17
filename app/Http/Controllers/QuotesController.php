af9a696e0993365c47c68020b3df83401bc31810
Adrian <sirianni.adrian@gmail.com>
2022-11-17 18:07:49 -0300

    Api Quotes

--- app/Http/Controllers/QuotesController.php
+++ app/Http/Controllers/QuotesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Quotes;
+use App\Models\StateQuote;
 use App\Models\User;
 
 class QuotesController extends Controller
@@ -17,6 +18,17 @@ class QuotesController extends Controller
     public function index()
     {
         $token=\App\Validaciones::validateToken();
+
+        $quotes= Quotes::all();
+
+        foreach ($quotes as $q) {
+           $q["state"]=StateQuote::find($q->state);
+           $q["id_professional"]=User::find($q->id_professional);
+           $q["id_visitor"]=User::find($q->id_visitor);
+        }
+
+        $token["response"]=response()->json($quotes,200);
+
         return $token["response"];
     }
 
@@ -93,7 +105,18 @@ class QuotesController extends Controller
      */
     public function show($id)
     {
-        //
+        $token=\App\Validaciones::validateToken();
+
+        $quote = Quotes::find($id);
+        
+        $quote->professional;
+        $quote->visitor;
+        $quote->state=StateQuote::find($quote->state);
+
+        $token["response"]=response()->json($quote,200);
+
+
+        return $token["response"];
     }
 
     /**
@@ -104,7 +127,7 @@ class QuotesController extends Controller
      */
     public function edit($id)
     {
-        //
+        
     }
 
     /**
@@ -116,7 +139,48 @@ class QuotesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $datos=$request->all();
+        $token=\App\Validaciones::validateToken();
+        $quote = Quotes::find($id);      
+
+        if($token["state"]){
+
+            //valida que el usuario del token sea el profesional para cambiar de estado
+            if($token["user"]->id===$quote->id_professional){
+                
+
+                $validator = Validator::make($request->all(), [
+                    'state' => [
+                        'required',
+                        'integer',
+                        function ($attribute, $value, $fail) {
+                           
+                            if ( $value<1 || $value >4) {
+                                $fail($attribute.' no valido (1-4 validos)');
+                            }
+                        },
+                    ]
+                ]);
+
+                if($validator->fails()){
+                    return response()->json($validator->errors(),400);
+                }
+
+                $quote->state=$datos["state"];
+                $quote->save();
+
+                $quote->state=StateQuote::find($quote->state);
+                               
+                $token["response"]=response()->json($quote, "200");
+            }else{
+                $token["response"]=response()->json(['usuario sin permisos para editar'], "403");
+            }
+        }
+
+        
+
+
+        return $token["response"];
     }
 
     /**
--- app/Models/Quotes.php
+++ app/Models/Quotes.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Quotes extends Model
 {
     use HasFactory;
+
+    public function professional()
+    {
+        return $this->hasOne(User::class,'id','id_professional');
+    }
+
+    public function visitor()
+    {
+        return $this->hasOne(User::class,'id','id_visitor');
+    }
+
+    public function state()
+    {
+        return $this->hasOne(StateQuote::class,'id','state');
+    }
 }
